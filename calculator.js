868296e2e613bbfa6506e5c856d6cc6db70d8345
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-07-18 00:35:27 -0300

    h

--- calculator.js
+++ calculator.js
@@ -5,23 +5,34 @@ function Calculator() {
     this._secondNumber = 0;
     this._plus = false;
     this._minus = false;
-    this._equal = 0
+    this._equal = 0;
 }
 
 Calculator.prototype.number = function(number) {
-    if (this._firstNumber === false && this._secondNumber === 0) return 0
-    if (this._firstNumber === false && this._secondNumber !== 0) {
+    if (this._firstNumber === false && this._secondNumber === 0 && number === undefined && this._equal === 0) return 0
+    if (this._firstNumber === false && this._secondNumber === 0 && number === 0 && this._equal === 0) return 0
+    if (this._firstNumber === false && this._secondNumber === 0 && this._equal === 0) {
         let first = this._secondNumber
         let second = number
         let concat = first.toString() + second.toString()
         this._secondNumber = parseInt(concat, 10);
-    }
-    return this._secondNumber;
+        return this._secondNumber;
+    } else {
+        let first = this._secondNumber
+        let second = number
+        let concat = first.toString() + second.toString()
+        this._secondNumber = parseInt(concat, 10);
+        return this._secondNumber;
+    }   
+    
+    
 }
 
 Calculator.prototype.plus = function() {
-    if (!this._firstNumber && this._secondNumber === 0) return 'Error'
+    if (!this._firstNumber && this._secondNumber === 0) return 0
     this._plus = true
+    this._firstNumber = this._secondNumber;
+    this._secondNumber = 0;
     return ' '
 }
 
@@ -34,15 +45,11 @@ Calculator.prototype.equal = function() {
     if (this._firstNumber === false && this._secondNumber !== 0) return this._secondNumber
     if (this._plus === true) {
         this._equal = this._firstNumber + this._secondNumber
-        this._secondNumber = this._firstNumber + this._secondNumber
+        this._secondNumber = this._equal
         this.plus === false
         return this._equal;
     }
-    if (this._minus === true) {
-        //this._secondNumber = this._firstNumber - this._secondNumber
-        this._equal = this._firstNumber - this._secondNumber
-        return this._equal;
-    }
+  
 }
 
 Calculator.prototype.clear = function() {
@@ -55,18 +62,60 @@ Calculator.prototype.clear = function() {
 }
 
 let qq =  new Calculator()
+
  console.log(qq.plus())
  console.log(qq.equal())
  console.log(qq.equal())
  console.log(qq.equal())
-  console.log(qq.number(0)) 
+  console.log(qq.number()) 
   console.log(qq.number(0)) 
   console.log(qq.number(0)) 
   console.log(qq.number(0)) 
   console.log(qq.clear())
   console.log(qq.number(2)) 
-
-//  console.log(qq.equal())
+  console.log(qq.number(2)) 
+  console.log(qq.number(2))
+  console.log(qq.number(2)) 
+  console.log(qq.number(0)) 
+  console.log(qq.number(0)) 
+  console.log(qq.number(1)) 
+  console.log(qq.number(9)) 
+  console.log(qq.number(9)) 
+  console.log(qq.number(9)) 
+  console.log(qq.number(9)) 
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.plus())
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.equal())
+  console.log(qq.clear())
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.number(1)) 
+  console.log(qq.clear())
+  console.log(qq.number(1)) 
+  console.log(qq.number(2)) 
+  console.log(qq.plus())
+  console.log(qq.number(1)) 
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.clear())
+  console.log(qq.number(1)) 
+  console.log(qq.number(0)) 
+  console.log(qq.plus())
+  console.log(qq.number(2)) 
+  console.log(qq.number(0)) 
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.equal())
+  console.log(qq.equal())
 //  console.log(qq.equal())
 //  console.log(qq.clear())
 //  console.log(qq.clear())
